4e757faaa8002c49b38facec6e21ca182f176a7a
jsakamotoIGJP <jsakamoto@infragistics.com>
2025-08-01 13:48:46 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using KB16998_IgbButton_AlignText;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor();
+
+await builder.Build().RunAsync();
